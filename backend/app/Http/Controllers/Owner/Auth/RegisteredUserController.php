df31b33acd1ed9b5c847071aa3c4d96b7de5f2b1
naoki-yamanoi <ti44nnyy999@gmail.com>
2023-02-13 20:07:49 +0900

    viewファイルでuser,owner,admin作成まで

--- backend/app/Http/Controllers/Owner/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Owner/Auth/RegisteredUserController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Owner\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Owner;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -20,7 +20,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        return view('owner.auth.register');
     }
 
     /**
@@ -32,20 +32,20 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.Owner::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
+        $owner = Owner::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        event(new Registered($owner));
 
-        Auth::login($user);
+        Auth::guard('owners')->login($owner);
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect(RouteServiceProvider::OWNER_HOME);
     }
 }
