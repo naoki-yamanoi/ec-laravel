d6640fdb4e2ebc535e937ce7d661bcc6c4d23f10
naoki-yamanoi <ti44nnyy999@gmail.com>
2023-02-19 15:00:34 +0900

    オーナー情報更新、フラッシュメッセージ修正

--- backend/resources/views/components/flash-message.blade.php
+++ backend/resources/views/components/flash-message.blade.php
@@ -1,12 +1,12 @@
-@props(['status'])
+@props(['status' => 'info'])
 
 @php
-if(session('status') === 'info') { $bgColor = 'bg-blue-300'; }
-if(session('status') === 'error') { $bgColor = 'bg-red-500'; }
+if($status === 'info') { $bgColor = 'bg-blue-300'; }
+if($status === 'error') { $bgColor = 'bg-red-500'; }
 @endphp
 
 @if (session('message'))
-    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+    <div class="{{ $bgColor }} w-1/2 mx-auto mb-2 p-2 text-white">
         {{ session('message') }}
     </div>
 @endif
